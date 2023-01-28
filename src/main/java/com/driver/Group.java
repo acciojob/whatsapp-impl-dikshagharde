2f115de9a82eea79323ad66b69317fb20fc148a9
dikshagharde <dikshagharde23@gmail.com>
2023-01-28 19:23:35 +0530

    whatsapp

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -4,4 +4,27 @@ public class Group {
     private String name;
     private int numberOfParticipants;
 
+    //constructor
+    public Group(String name, int numberOfParticipants) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    //getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
 }
+
